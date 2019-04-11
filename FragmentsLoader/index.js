eaefc8ed6d19c542953261d754a906d9c953cd91
Miroslav Kubus <MiroslavK@kentico.com>
2019-04-11 10:44:59 +0200

    KA-523 Optimize creation of the code samples content item

--- FragmentsLoader/index.js
+++ FragmentsLoader/index.js
@@ -1,7 +1,12 @@
 const { loadDataFromBlobStorage } = require('./utils/blobStorageLoader');
+const { getGroupedBy } = require('../shared/utils/getGroupedBy');
 
 module.exports = async function (context) {
     const url = context.bindingData.blobStorageUrl;
+    const loadedData = await loadDataFromBlobStorage(url);
 
-    return loadDataFromBlobStorage(url);
+    return {
+        mode: loadedData.mode,
+        codeFragments: getGroupedBy(loadedData.codeFragments, 'identifier'),
+    }
 };
