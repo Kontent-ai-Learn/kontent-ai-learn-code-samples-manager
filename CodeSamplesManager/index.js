02dbe7781357bef4bdfc2cb2b87b69b2bb83718b
Miroslav Kubus <MiroslavK@kentico.com>
2019-04-25 17:44:31 +0200

    KA-538 Add correct logging of errors on Azure

--- CodeSamplesManager/index.js
+++ CodeSamplesManager/index.js
@@ -8,15 +8,20 @@ const {
 } = require('./utils/codeSamplesHandlers');
 
 module.exports = async function (context) {
-    setupAzureStorage();
-    setupKenticoCloud();
+    try {
+        setupAzureStorage();
+        setupKenticoCloud();
 
-    const codeSamplesList = context.bindingData.codeSamplesList;
+        const codeSamplesList = context.bindingData.codeSamplesList;
 
-    if (codeSamplesList.length !== 0) {
-        const codeSamplesItemCodename = codeSamplesList[0].identifier;
+        if (codeSamplesList.length !== 0) {
+            const codeSamplesItemCodename = codeSamplesList[0].identifier;
 
-        await updateCodeSampleInfoAsync(codeSamplesList);
-        await updateCodeSamplesItemAsync(codeSamplesItemCodename);
+            await updateCodeSampleInfoAsync(codeSamplesList);
+            await updateCodeSamplesItemAsync(codeSamplesItemCodename);
+        }
+    } catch (error) {
+        /** This try-catch is required for correct logging of exceptions in Azure */
+        throw `Message: ${error.message} \nStack Trace: ${error.stack}`;
     }
 };
