32dce4e50a737ff8b264d48f61429c9847c32084
Matus Backor <MatusB@kentico.com>
2019-04-26 08:38:42 +0200

    Add 429 to retry status codes

--- shared/utils/getContentManagementClient.js
+++ shared/utils/getContentManagementClient.js
@@ -5,6 +5,7 @@ function getContentManagementClient() {
     return new ContentManagementClient({
         projectId: configuration.kenticoProjectId,
         apiKey: configuration.kenticoContentManagementApiKey,
+        retryStatusCodes: [429, 500]
     });
 }
 
