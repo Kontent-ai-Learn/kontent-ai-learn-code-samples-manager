b7462818c5df3ae3d47835445c9f123fc3b6b609
Miroslav Kubus <MiroslavK@kentico.com>
2019-04-29 12:36:53 +0200

    KA-525 Set up custom retry policy

--- shared/utils/getContentManagementClient.js
+++ shared/utils/getContentManagementClient.js
@@ -5,6 +5,9 @@ function getContentManagementClient() {
     return new ContentManagementClient({
         projectId: configuration.kenticoProjectId,
         apiKey: configuration.kenticoContentManagementApiKey,
+        /* Ensures we don't hit the requests per minute API limit */
+        retryAttempts: 9,
+        /* To ensure we retry correct refused requests. */
         retryStatusCodes: [429, 500]
     });
 }
